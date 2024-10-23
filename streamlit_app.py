f251af654bab5868044ad863a4ba6a36edc3cb00
clyfferibeiro <165920262+clyfferibeiro@users.noreply.github.com>
2024-10-23 18:41:18 -0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,41 +14,19 @@ from email.mime.text import MIMEText
 import smtplib
 import email.message
 
+def clear_text():
+    st.session_state.my_text = st.session_state.widget
+    st.session_state.widget = ""
+
 st.title('Enviar Mensagem Anônima 💌 🚀')
+
+#st.image("logo.png")
 
 st.markdown("""
 **Escreva a mensagem no campo abaixo e clique em enviar.**
 """)
 
-mensagem = st.text_area('**Digite sua mensagem:**', height=200)
-
-# # Taking inputs
-# email_sender = st.text_input('From', 'clyffe.ribeiro@escolajayme.com', disabled=True)
-# email_receiver = st.text_input('To')
-# subject = st.text_input('Subject')
-# body = st.text_area('Body')
-
-# # Hide the password input
-# password = st.text_input('Password', type="password", disabled=False)  
-
-# if st.button("Send Email"):
-#     try:
-#         import smtplib, ssl
-
-#     port = 465  # For SSL
-#     #password = input("Type your password and press enter: ")
-
-#     # Create a secure SSL context
-#     context = ssl.create_default_context()
-
-#     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-#         server.login(email_sender, password)
-#         # TODO: Send email here
-
-#         st.success('Email sent successfully! 🚀')
-#     except Exception as e:
-#         st.error(f"Failed to send email: {e}")
-
+mensagem = st.text_area('**Digite sua mensagem:**', height=200,  key='widget')
 
 
 def enviar_email(mensagem):  
@@ -57,7 +35,8 @@ def enviar_email(mensagem):
     msg = email.message.Message()
     msg['Subject'] = "Nova Mensagem Anônima - Antibullying"
     msg['From'] = 'mensagem_anonima_bullying@escolajayme.com'
-    msg['To'] = 'clyffe.ribeiro@escolajayme.com', 'clyffe.assis@gmail.com'
+    recipients = ["clyffe.assis@gmail.com", "clyffe.ribeiro@escolajayme.com"]
+    msg['To'] = ",".join(recipients)
     password = 'ncmkfwsfzjgvkdkm' 
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email )
@@ -66,16 +45,16 @@ def enviar_email(mensagem):
     s.starttls()
     # Login Credentials for sending the mail
     s.login(msg['From'], password)
-    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    s.sendmail(msg['From'], ["clyffe.assis@gmail.com", "clyffe.ribeiro@escolajayme.com"], msg.as_string().encode('utf-8'))
     
 
 
 if st.button("Enviar"):
     try:
         enviar_email(mensagem)
-        st.success('Mensagem Enviada Com Sucesso! 🚀')
-        with st.container():
-            st.write("**Mensagem enviada:**")
-            st.write(mensagem)
+        st.success(f'Mensagem Enviada Com Sucesso! 🚀')
+        st.write("**Mensagem enviada:**")
+        with st.container(border=True):            
+            st.write(f'**{mensagem}**')
     except:
-        st.error(f"Failed to send email: {e}")
+        st.error(f"Erro no envio da mensagem!")
