37cde671240cd7b702a1321387624645dc0f9cf4
clyfferibeiro <165920262+clyfferibeiro@users.noreply.github.com>
2024-10-23 17:27:35 -0300

    Add files via upload

---
+++ mensagem_anonima_bullying/streamlit_app.py
@@ -0,0 +1,78 @@
+""" 
+If you've two-step verification enabled, your regular password won't work. Instead, generate an app-specific password:
+
+- Go to your Google Account.
+- On the left navigation panel, click on "Security."
+- Under "Signing in to Google," select "App Passwords." You might need to sign in again.
+- At the bottom, choose the app and device you want the app password for, then select "Generate."
+- Use this app password in your Streamlit app.
+
+"""
+
+import streamlit as st
+from email.mime.text import MIMEText
+import smtplib
+import email.message
+
+st.title('Enviar Mensagem Anônima 💌 🚀')
+
+st.markdown("""
+**Escreva a mensagem no campo abaixo e clique em enviar.**
+""")
+
+mensagem = st.text_area('Digite sua mensagem')
+
+# # Taking inputs
+# email_sender = st.text_input('From', 'clyffe.ribeiro@escolajayme.com', disabled=True)
+# email_receiver = st.text_input('To')
+# subject = st.text_input('Subject')
+# body = st.text_area('Body')
+
+# # Hide the password input
+# password = st.text_input('Password', type="password", disabled=False)  
+
+# if st.button("Send Email"):
+#     try:
+#         import smtplib, ssl
+
+#     port = 465  # For SSL
+#     #password = input("Type your password and press enter: ")
+
+#     # Create a secure SSL context
+#     context = ssl.create_default_context()
+
+#     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+#         server.login(email_sender, password)
+#         # TODO: Send email here
+
+#         st.success('Email sent successfully! 🚀')
+#     except Exception as e:
+#         st.error(f"Failed to send email: {e}")
+
+
+
+def enviar_email(mensagem):  
+    corpo_email = mensagem
+
+    msg = email.message.Message()
+    msg['Subject'] = "Nova Mensagem Anônima - Antibullying"
+    msg['From'] = 'mensagem_anonima_bullying@escolajayme.com'
+    msg['To'] = 'clyffe.ribeiro@escolajayme.com'
+    password = 'ncmkfwsfzjgvkdkm' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    
+
+
+if st.button("Enviar"):
+    try:
+        enviar_email(mensagem)
+        st.success('Mensagem Enviada Com Sucesso! 🚀')
+    except:
+        st.error(f"Failed to send email: {e}")
