cf5d210e397c4d7cec0b523366bb94d2d871b1d6
clyfferibeiro <165920262+clyfferibeiro@users.noreply.github.com>
2024-10-23 22:32:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ from email.mime.text import MIMEText
 import smtplib
 import email.message
 
-st.set_page_config(page_title="App Mensagem Anônima", page_icon="💌")
+st.set_page_config(page_title="App Mensagem Anônima", page_icon="📋")
 
 def clear_text():
     st.session_state.my_text = st.session_state.widget
